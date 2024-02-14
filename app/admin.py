e95fedf8c015ebaa8c6a0e8ea423f89651fb43de
Diyorbek <majidovdiyorbek741@gmail.com>
2024-02-14 09:57:34 +0500

    admin translation fields model

--- app/admin.py
+++ app/admin.py
@@ -1,30 +1,21 @@
 from django.contrib import admin
 from modeltranslation.admin import TranslationAdmin
-from .models import Journal, JournalCategory, InfoSendJournal,Articles
+from .models import Journal, JournalCategory, InfoSendJournal,Articles,Journal
 
 @admin.register(JournalCategory)
 class CategoryTranslationAdmin(TranslationAdmin):
     list_display = ('name',)
 
-# class InfoSendJournalAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'journal_category')
-#     search_fields = ('title',)
-#     ordering = ('title',)
+
+@admin.register(InfoSendJournal)
+class InfoSendJournalTranslationAdmin(TranslationAdmin):
+    list_display = ("title",)
+    search_fields = ('title',)
+    ordering = ('title',)
+
+@admin.register(Journal)
+class JournalTranslationAdmin(TranslationAdmin):
+    list_display = ('name',)
 
 
-# class JournalAdmin(admin.ModelAdmin):
-#     list_display = ('name', 'journal_category')
-#     search_fields = ('name',)
-#     ordering = ('name',)
-
-# # class JournalCategoryAdmin(admin.ModelAdmin):
-# #     list_display = ('name',)
-# #     search_fields = ('name',)
-# #     ordering = ('name',)
-    
-
-
-# admin.site.register(InfoSendJournal, InfoSendJournalAdmin)
-# admin.site.register(Journal, JournalAdmin)
-# # admin.site.register(JournalCategory, JournalCategoryAdmin)
-# admin.site.register(Articles)
+admin.site.register(Articles)
--- app/translation.py
+++ app/translation.py
@@ -1,6 +1,14 @@
 from modeltranslation.translator import register, TranslationOptions
-from .models import JournalCategory
+from .models import JournalCategory,Journal,InfoSendJournal
 
 @register(JournalCategory)
 class JournalCategoryTranslationOptions(TranslationOptions):
     fields = ('name', 'info') 
+
+@register(Journal)
+class JournalTranslationOptions(TranslationOptions): 
+    fields = ('name',)
+
+@register(InfoSendJournal)
+class  InfoSendJournalTranslationOptions(TranslationOptions):
+    fields = ('title','body',)
