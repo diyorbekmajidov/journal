b12bb3e0dd0d613da11e4402ad9df4490efd97f6
Diyorbek <majidovdiyorbek741@gmail.com>
2024-02-21 17:40:49 +0500

    blocktranslate'

--- app/translation.py
+++ app/translation.py
@@ -1,5 +1,5 @@
 from modeltranslation.translator import register, TranslationOptions
-from .models import JournalCategory,Journal,InfoSendJournal
+from .models import JournalCategory,Journal,InfoSendJournal,Articles
 
 @register(JournalCategory)
 class JournalCategoryTranslationOptions(TranslationOptions):
@@ -12,3 +12,10 @@ class JournalTranslationOptions(TranslationOptions):
 @register(InfoSendJournal)
 class  InfoSendJournalTranslationOptions(TranslationOptions):
     fields = ('title','body',)
+
+
+@register(Articles)
+class ArticlesTranslationOptions(TranslationOptions):
+    fields = ('title_art','abstract','Keywords','author_1','info_auth1','author_2','info_auth2',
+              'author_3','info_auth3','author_4','info_auth4'
+              )
